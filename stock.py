c40578e9c4721289d388d1084f8a2569f4d0219e
AdyatamaMH <114073452+AdyatamaMH@users.noreply.github.com>
2023-01-15 20:38:09 +0700

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,15 +1,16 @@
-import streamlit as st
-# streamlit acts as a web application
-
 from datetime import date
 # the datetime module provides classes for manipulating dates and times, while the date class is used to represent a specific date
+
+from prophet import Prophet
+# prophet acts as a main class of this code
+
+import streamlit as stock
+# streamlit acts as a web application
 
 import yfinance as yf
 # The yfinance library is a Python library 
 # for accessing financial data from Yahoo Finance, including stock market data, financial news, and historical data.
 
-from prophet import Prophet
-# prophet acts as a main class of this code
 
 from prophet.plot import plot_plotly
 # The prophet is a library for time series forecasting in Python, which is built on top of pystan.
@@ -18,25 +19,25 @@ from plotly import graph_objs as go
 # plotly is a library for an interactive graph
 
 START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
 # START is assigned a string value of "2015-01-01" which represents a date in format "YYYY-MM-DD". 
 # This variable is used to specify the starting date of a certain range or period of time.
+TODAY = date.today().strftime("%Y-%m-%d")
 # TODAY is assigned a value returned by the date.today().strftime("%Y-%m-%d") function.
 # This variable is used to specify the end date of a certain range or period of time.
 
 
-st.title('Stock Prediction Application')
+stock.title('Stock Prediction Application')
 # Title for the streamlit application
 
 stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
+selected_stock = stock.selectbox('Select dataset for prediction', stocks)
 #Allows the user to choose the stock provided from the list.
 
-n_years = st.slider('Years of prediction:', 1, 4)
+n_years = stock.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 # allows the user to select the number of years for which they would like to see the Stock Prediction.
 
-@st.cache
+@stock.cache
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
@@ -44,13 +45,13 @@ def load_data(ticker):
 # This function loads the historical data of a stock between a specified date range, and it uses caching to avoid loading the data again
 
 
-data_load_state = st.text('Loading data...')
+data_load_state = stock.text('Loading data...')
 data = load_data(selected_stock)
 data_load_state.text('Loading data... done!')
 # This function only displays the loading message for the user.
 
-st.subheader('Raw data')
-st.write(data.tail())
+stock.subheader('Raw data')
+stock.write(data.tail())
 # Allows the user to see a sample of the loaded data.
 
 
@@ -61,7 +62,7 @@ def plot_raw_data():
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
     fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
+    stock.plotly_chart(fig)
 
 plot_raw_data()
 # This code defines a function that plots the raw data of the stock prices and display it on web page.
@@ -78,14 +79,14 @@ forecast = m.predict(future)
 # This code creates an instance of the Prophet class and makes a prediction for the specified number of days using the model and assign the data to the variable.
 
 # Show and plot forecast
-st.subheader('Prediction data')
-st.write(forecast.tail())
+stock.subheader('Prediction data')
+stock.write(forecast.tail())
 
-st.write(f'Prediction plot for {n_years} years')
+stock.write(f'Prediction plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
+stock.plotly_chart(fig1)
 
-st.write("Prediction components")
+stock.write("Prediction components")
 fig2 = m.plot_components(forecast)
-st.write(fig2)
+stock.write(fig2)
 # This code purpose is to display the stock prediction data.
